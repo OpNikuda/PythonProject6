a2a8ed5379080b5d5090b042025f1b3e99d3d5ac
Nikonorov.M <maximnikonorov957@gmail.com>
2025-04-23 12:58:31 +0300

    Проделана работа над ошибками

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,8 @@
 import json
-from importlib.resources import files
 
-
-def get_json() -> list[dict]:
+def get_json(filename) -> list[dict]:
     try:
-        # Используем importlib.resources для доступа к файлу в пакете
-        data_package = files('data')
-        with (data_package / 'operations.json').open('r', encoding='utf-8') as file:
+        with open(filename, 'r', encoding='utf-8') as file:
             return json.load(file)
     except (FileNotFoundError, json.JSONDecodeError, PermissionError):
         return []
